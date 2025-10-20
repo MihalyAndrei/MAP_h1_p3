9e01dda32143a148e4236ef3f6fe8449649fd826
Mihaly Cozmuta Andrei <andrei.mihaly@stud.ubbcluj.ro>
2025-10-20 20:55:32 +0300

    finished subtraction function

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        int[] number1={1,3,0,0,0,0};
         int[] number2={8,7,0,0,0,0};
+        int[] number1={1,3,0,0,0,0};
 
         //1
         int[] newnumber1=Operations.Addition(number1,number2);
         for(int i=0;i<newnumber1.length;i++){
             System.out.print(newnumber1[i]);
         }
-
+        System.out.println();
         //2
-        int[] newnumber2=Operations
+        int[] newnumber2=Operations.Subtraction(number1,number2);
+        for(int i=0;i<newnumber2.length;i++){
+            System.out.print(newnumber2[i]);
+        }
+
+
     }
 }
--- src/Operations.java
+++ src/Operations.java
@@ -27,4 +27,37 @@ public class Operations {
         }
         return newnumber;
     }
+
+    public static int[] Subtraction(int[] number1, int[] number2) {
+        int maxLength = Math.max(number1.length, number2.length);
+        int remaining = 0;
+        int[] newnumber = new int[maxLength];
+
+        for (int i=0;i<maxLength;i++) {
+            int c1=0;
+            int c2=0;
+
+            int pos1=number1.length-i-1;
+            if(pos1>=0){
+                c1=number1[pos1];
+            }
+
+            int pos2=number2.length-i-1;
+            if(pos2>=0){
+                c2=number2[pos2];
+            }
+
+            int dif=c1-c2-remaining;
+            if(dif<0){
+                dif=dif+10;
+                remaining=1;
+            }
+            else{
+                remaining=0;
+            }
+
+            newnumber[newnumber.length-1-i]=dif;
+        }
+        return newnumber;
+    }
 }
